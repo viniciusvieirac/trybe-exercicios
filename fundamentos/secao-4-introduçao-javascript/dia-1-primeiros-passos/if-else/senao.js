e33126bc99bb4cd3386e43a4d785531a049e655a
Vinicius <vini.clau0@gmail.com>
2022-11-27 11:12:09 -0300

    adiciona if else

---
+++ fundamentos/secao-4-introduçao-javascript/dia-1-primeiros-passos/if-else/senao.js
@@ -0,0 +1,11 @@
+const nota =68;
+
+if (nota >= 80){
+    console.log("Parabéns, você foi aprovado!")
+}
+else if (nota < 80 && nota >= 60){
+    console.log("Você está na nossa lista de espera")
+}
+else if (nota < 60){
+    console.log("Você foi reprovada(o)")
+}
---
+++ fundamentos/secao-4-introduçao-javascript/dia-1-primeiros-passos/tipos primitivos, tipagem dinamica e operaçoes aritmeticas/index.js
@@ -0,0 +1,6 @@
+const base = 5;
+const height = 8;   
+const area = base * height;
+const perimeter = 5 + 5 + 8 + 8;
+ console.log(area);
+ console.log(perimeter);
