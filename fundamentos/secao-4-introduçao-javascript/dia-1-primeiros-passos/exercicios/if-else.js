27cb8fd92b64fd826df6f6bedd01e468d26b3bb1
Vinicius <vini.clau0@gmail.com>
2022-11-27 13:14:46 -0300

    resolve exercicio 2

---
+++ fundamentos/secao-4-introduçao-javascript/dia-1-primeiros-passos/exercicios/if-else.js
@@ -0,0 +1,9 @@
+const a = 5;
+const b = 10;
+
+if (a > b){
+console.log("a é maior que b");
+}
+else {
+    console.log("B é maior que A")
+};
---
+++ fundamentos/secao-4-introduçao-javascript/dia-1-primeiros-passos/exercicios/if-else2.js
@@ -0,0 +1,13 @@
+const a = 5;
+const b = 10;
+const c = 15;
+
+if (a > b && a > c) {
+    console.log('O maior número é: ' + a + ' (a)');
+  } 
+  else if (b > a && b > c) {
+    console.log('O maior número é: ' + b + ' (b)');
+  } 
+  else {
+    console.log('O maior número é: ' + c + ' (c)');
+  };
