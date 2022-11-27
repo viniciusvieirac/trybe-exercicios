bd71a7d748f298da4ef6c1474877578d07c06ed1
Vinicius <vini.clau0@gmail.com>
2022-11-27 12:17:55 -0300

    fixa operadores logicos

---
+++ fundamentos/secao-4-introduçao-javascript/dia-1-primeiros-passos/operadores logicos/padaria.js
@@ -0,0 +1,7 @@
+let weekDay = "domingo";
+if (weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay === 'quarta-feira' || weekDay === 'quinta-feira'){
+    console.log("Oba, mais um dia de aprendizado na Trybe");
+}
+else{
+    console.log("FINALMENTE, descanso merecido");
+}
