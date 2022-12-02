edaab638aa98913d2ca83cea26026c99007ea533
vieira <vini.clau0@gmail.com>
2022-12-01 21:34:34 -0300

    resolve exercicio maior e menor palavra em um array

---
+++ fundamentos/secao-4-introduçao-javascript/dia-3-logica-programacao/maiorpalavra.js
@@ -0,0 +1,38 @@
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let biggestWord = [];
+// let smallestWord = [];
+
+// for (let index = 0; index < array.length; index += 1){
+//     if(array[index].length > biggestWord.length){
+//         biggestWord = array[index];
+//  }
+// }; 
+
+// for (let index = 0; index < array.length; index += 1) {
+//     if (array[index].length < smallestWord.length) {
+//       smallestWord = array[index];
+//     }
+//   }
+
+// console.log(biggestWord);
+// console.log(smallestWord);
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let biggestWord = [];
+let smallestWord = array[0];
+
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length > biggestWord.length) {
+    biggestWord = array[index];
+  }
+}
+
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length < smallestWord.length) {
+    smallestWord = array[index];
+  }
+}
+
+console.log(biggestWord);
+console.log(smallestWord);
