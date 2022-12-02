a725bf2504482559dcd1c55a09066ef839018ffc
vieira <vini.clau0@gmail.com>
2022-12-02 13:12:37 -0300

    fazendo exercicios

--- fundamentos/secao-4-introduçao-javascript/dia-4-objetos-e-funcoes/exercicios/funcoes/menorvalor.js
+++ fundamentos/secao-4-introduçao-javascript/dia-4-objetos-e-funcoes/exercicios/funcoes/menorvalor.js
@@ -1,1 +1,10 @@
-function
+function smallNum (array){
+let smallestNum = 0;
+for (const key in array) {
+    if (array[smallestNum] > array[key]){
+        smallestNum = key
+    }
+}
+return smallestNum
+};
+console.log(smallNum([2, 4, 6, 7, 10, 0, -3]));
