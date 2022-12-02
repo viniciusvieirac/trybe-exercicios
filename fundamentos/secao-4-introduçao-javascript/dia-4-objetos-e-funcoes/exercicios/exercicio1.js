baabc51fad42cea1b5f6dadbf7bc4665bcd13bae
vieira <vini.clau0@gmail.com>
2022-12-01 23:14:15 -0300

    finaliza exercicios for/in

---
+++ fundamentos/secao-4-introduçao-javascript/dia-4-objetos-e-funcoes/exercicios/exercicio1.js
@@ -0,0 +1,17 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+  info.personagem('e Tio Patinhas');
+  info.recorrente = "Sim"
+//   console.log("Bem vinda, " + info.personagem);
+//   console.log(info);
+
+// for (let index in info){
+//     console.log(index);
+// };
+
+// for(let index in info){
+//     console.log(info[index]);
+// };
