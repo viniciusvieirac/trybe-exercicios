7a66795d64bfdffe8bb530ec2449ce1412001750
vieira <vini.clau0@gmail.com>
2022-12-07 19:08:44 -0300

    começa seçao 5 dia 1

---
+++ fundamentos/secao-5-javascript-DOM-EVENTOS-WEB-STORAGE/teste/script.js
@@ -0,0 +1,22 @@
+const header = document.getElementById("header-container");
+header.style.backgroundColor = 'rgb(0, 176, 105)';
+
+const emergencyTasks = document.getElementsByClassName("emergency-tasks")[0];
+emergencyTasks.style.backgroundColor = 'rgb(255, 159, 132)';
+
+const emergencyTasksHeaders = document.querySelectorAll('.emergency-tasks h3');
+for (index = 0; index < emergencyTasksHeaders.length; index += 1){
+    emergencyTasksHeaders[index].style.backgroundColor = 'rgb(165, 0, 243)';
+};
+
+const noEmergencyTasksHeaders2 = document.querySelector('.no-emergency-tasks')
+noEmergencyTasksHeaders2.style.backgroundColor = 'rgb(249, 219, 94)';
+
+const noEmergencyTasksHeaders = document.querySelectorAll('.no-emergency-tasks h3');
+for (index = 0; index < noEmergencyTasksHeaders.length; index += 1){
+    noEmergencyTasksHeaders[index].style.backgroundColor = 'rgb(35, 37, 37)';
+};
+
+const footContainer = document.getElementById("footer-container");
+footContainer.style.backgroundColor = 'rgb(0, 53, 51)';
+
